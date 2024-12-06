b3802f06cc223e21c98d8e9dc8a4f3663aba166b
CristinaR08 <cris.04082009@hotmail.com>
2024-12-06 00:43:38 +0000

    Taller 15: Directiva v-model

--- js/appFrases.js
+++ js/appFrases.js
@@ -1,22 +1,53 @@
 //arreglo de objetos Frase
 const frases = [
-    {frase: 'El que se conquista a sí mismo es el guerrero más poderoso' ,autor: 'Confucio'},
-    {frase: 'Un hombre con coraje hace una mayoría' ,autor: 'Andrew Jackson'},
-    {frase: 'El hombre que comete un error y no lo corrige, está cometiendo otro error' ,autor: 'Confucio'},
-    {frase: 'Todo es divertido, siempre y cuando le ocurra a otra persona' ,autor: 'Armando '},
-    {frase: 'Ojos que no ven... amigas que te lo cuentan todo.' ,autor: 'Jairo Chiles'},
-    {frase: 'La inteligencia me persigue, pero yo soy más rápido' ,autor: 'Elvis'},
+    { frase: 'El que se conquista a sí mismo es el guerrero más poderoso', autor: 'Confucio' },
+    { frase: 'Un hombre con coraje hace una mayoría', autor: 'Andrew Jackson' },
+    { frase: 'El hombre que comete un error y no lo corrige, está cometiendo otro error', autor: 'Confucio' },
+    { frase: 'Todo es divertido, siempre y cuando le ocurra a otra persona', autor: 'Armando ' },
+    { frase: 'Ojos que no ven... amigas que te lo cuentan todo.', autor: 'Jairo Chiles' },
+    { frase: 'La inteligencia me persigue, pero yo soy más rápido', autor: 'Elvis' },
 ]
 
 const app = Vue.createApp({
     methods: {
+        agregarFrase() {
+            console.log(this.frase);
+            console.log(this.autor);
 
+            const nuevaFrase = {
+                frase: this.frase,
+                autor: this.autor
+            }
+            this.listaFrases.unshift(nuevaFrase);
+        },
+        agregarFraseFin() {
+            console.log(this.frase);
+            console.log(this.autor);
+
+            const nuevaFrase = {
+                frase: this.frase,
+                autor: this.autor
+            }
+            this.listaFrases.push(nuevaFrase);
+        },
+        eventoKeyPressModificador() {
+                console.log('Evento');
+                // console.log(event);
+                // console.log(charCode);
+                // console.log(cancelable);
+                // console.log(key);
+                // console.log(keyCode);
+                // console.log(target.baseURI);
+                this.agregarFraseFin();
+        }
     },
 
     data() {
         return {
-            listaFrases:frases
-
+            listaFrases: frases,
+            frase: null,
+            //autor: 'sin autor',
+            autor: null,
         }
     }
 })
